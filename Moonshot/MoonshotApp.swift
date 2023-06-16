f35c19c9c4de0571821724386388bc5c2a1c24ea
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-06-16 09:03:14 +0200

    first commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ivan Trajanovski on 16.06.23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
